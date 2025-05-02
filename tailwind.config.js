aa3f3715c1651554333eb035a29c0118d74bba86
Parth <86861599+GamerArjun@users.noreply.github.com>
2025-05-03 02:34:17 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  extend: {
-    animation: {
-      'fade-in': 'fadeIn 1s ease-out forwards',
-    },
-    keyframes: {
-      fadeIn: {
-        '0%': { opacity: 0, transform: 'translateY(20px)' },
-        '100%': { opacity: 1, transform: 'translateY(0)' },
-      },
-    },
+    extend: {},
   },
-};
   plugins: [],
 };
