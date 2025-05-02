c8b06949f1edfd30bfc7ab9ddc6d8d5e46186c2a
Parth <86861599+GamerArjun@users.noreply.github.com>
2025-05-03 02:24:21 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+theme: {
+  extend: {
+    animation: {
+      'fade-in': 'fadeIn 1s ease-out forwards',
+    },
+    keyframes: {
+      fadeIn: {
+        '0%': { opacity: 0, transform: 'translateY(20px)' },
+        '100%': { opacity: 1, transform: 'translateY(0)' },
+      },
+    },
   },
-  plugins: [],
-};
+}
